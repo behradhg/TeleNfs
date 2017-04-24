ef713d6247593e7a5878c02c9d12cf3ded8b89c7
BEHRAD <behrad831@yahoo.com>
2017-04-24 20:15:47 +0430

    By Behrad

---
+++ libs/tg-cli.lua
@@ -0,0 +1,11 @@
+
+default_profile = "default";
+
+default = {
+  config_directory = "/root/bet/data/telegram-cli";
+  auth_file = "/root/bet/data/telegram-cli/auth";
+  test = false;
+  msg_num = true;
+  log_level = 2;
+};
+
